3fb9f2aaba2d1e964e5f1107fd78cf0c4196eebe
yazcity <71519718+yazcity@users.noreply.github.com>
2022-03-23 13:26:57 +0400

    Delete index.c1118a87.js

--- assets/index.c1118a87.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./app.a6ac607b.js";import{f as t,l as s,o as a,c,e as n}from"./vendor.b93014b3.js";const i=t({setup(r){return s({title:"Blog - Liyas Thomas"}),(p,_)=>{const e=o;return a(),c("div",null,[n(e)])}}});export{i as default};
